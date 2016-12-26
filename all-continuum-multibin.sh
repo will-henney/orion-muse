6066b3ba72ab847b4600e62e3c621f9a0adf3883
William Henney <will@henney.org>
2016-12-26 19:23:34 +0000

    Update some scripts

--- all-continuum-multibin.sh
+++ all-continuum-multibin.sh
@@ -1,5 +1,6 @@
-linelist=LineMaps/continuum-*[0-9][0-9][0-9][0-9].fits
+linelist=LineMaps/continuum-*-[0-9][0-9][0-9][0-9].fits
+D=../multibin-maps
 for line in $linelist; do
     echo "Processing $line"
-    time python multibin-map.py $line > ${line}-multibin.log
+    time python $D/multibin-map.py $line > ${line}-multibin.log 2>&1
 done
--- batch-ew.sh
+++ batch-ew.sh
@@ -1,3 +1,3 @@
-for file in LineMaps/linesum*[0-9][0-9][0-9][0-9]$1-bin*.fits; do
+for file in LineMaps/linesum*-[0-9][0-9][0-9][0-9]$1-bin*.fits; do
     python muse-ew.py $file
 done
